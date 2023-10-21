956f9f2ac726c57bece3fd31a43d6f53aa298c89
ryosuke-horie <ryosuke03140@gmial.com>
2023-10-21 12:13:56 +0900

    イベントオブジェクトの受け取り

---
+++ directive-on-param-event/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const mousePointX = ref(0)
+const mousePointY = ref(0)
+
+const onMousemove = (event: MouseEvent): void => {
+  mousePointX.value = event.clientX
+  mousePointY.value = event.clientY
+}
+</script>
+
+<template>
+  <section>
+    <img src="public/logo.svg" alt="Vueのロゴ" width="200" v-on:mousemove="onMousemove" />
+    <p>ポイントの位置: x={{ mousePointerX }}; y={{ mousePointerY }}</p>
+  </section>
+</template>
